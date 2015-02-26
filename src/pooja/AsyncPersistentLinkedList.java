07aa4942882857bc65903cf7ece0c0e753e2a502
Fabrice Medio <fmedio@gmail.com>
2015-02-26 12:09:11 -0800

    AsyncPersistentLinkedList

---
+++ src/pooja/AsyncPersistentLinkedList.java
@@ -0,0 +1,19 @@
+package pooja;
+
+import java.util.concurrent.CompletableFuture;
+
+public class AsyncPersistentLinkedList {
+
+    public CompletableFuture<Void> append(int value) {
+        return null;
+    }
+
+    public CompletableFuture<Integer> size() {
+        return null;
+    }
+
+    public CompletableFuture<Integer> get(int n) {
+        return null;
+    }
+
+}
