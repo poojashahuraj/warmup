be141f3799cfa2429b3691f5bce4abba8cc7d859
poojashahuraj <pooja@formationds.com>
2015-03-04 14:30:04 -0700

    create bucket, key get bucket,key done

--- python/monitor.py
+++ python/monitor.py
@@ -1,3 +1,4 @@
+__author__ = 'parallels'
 import requests
 import boto
 from boto.s3.connection import OrdinaryCallingFormat
@@ -23,19 +24,41 @@ def execute(user_token, host, port, secure):
                                  port=port,
                                  is_secure=secure,
                                  calling_format=boto.s3.connection.OrdinaryCallingFormat())
-    buckets = connection.get_all_buckets()
-    for bucket in buckets:
-        print(str(bucket))
-
-    # pick a bucket name ("canary")
+    # pick a bucket name ("canary") hello
     # create bucket if it doesn't exist
-    # clean up contents of buckets
+    bucket_name = 'canary'
+    bucket1 = connection.create_bucket(bucket_name)
+
+    bucket = connection.get_bucket(bucket_name)
+    if bucket:
+        print('Bucket (%s) already exists' % bucket_name)
+        # clean up contents of buckets
+        for key in bucket.list():
+            bucket.delete_key(key)
+            print ("key %s deleted "%key)
+        connection.delete_bucket(bucket_name)
+
     # PUT a small object
+    bucket = connection.create_bucket(bucket_name)
+    key = bucket.new_key("hello.txt")
+    key.set_metadata("Content-Type", "txt")
+    key.set_contents_from_string("hello world !")
+
     # GET the object, make sure size and metadata match
+    for key in bucket.list():
+        print "{name}\t{size}\t{modified}".format(
+            name = key.name,
+            size = key.size,
+            modified = key.last_modified,
+            )
+
     # PUT a larger object
+    keyLarge = bucket.new_key("largeKey.txt")
+    keyLarge.set_contents_from_filename('/home/parallels/test.txt')
     # GET the object, make sure size and metadata match
-    # exit 0
-    # if anything goes wrong, exit 42
+
+
+
 
 def main():
     user = "admin"
@@ -46,6 +69,9 @@ def main():
     user_token = get_user_token(user, password, host, omPort, secure)
     execute(user_token, "us-east.formationds.com", 8443, 1)
 
-
 if __name__ == '__main__':
     main()
+
+
+    # exit 0
+    # if anything goes wrong, exit 42
